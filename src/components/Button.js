9175b53cc4859c929103c175b419d4113d0e127d
PradyumnNair <51204441+PradyumnNair@users.noreply.github.com>
2020-08-10 20:11:47 +0530

    Update Button.js

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
-const Button = ({ label }) => (
-  <button className="button">
+const Button = ({ label ,href}) => (
+  <button href={href} className="button">
     {label}
   </button>
 );
