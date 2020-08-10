56802462477521fe9aae89b6d986d1a8fec98bc0
PradyumnNair <51204441+PradyumnNair@users.noreply.github.com>
2020-08-10 19:53:07 +0530

    Update ProductBenefits.js

--- src/components/ProductBenefits.js
+++ src/components/ProductBenefits.js
@@ -6,16 +6,16 @@ const ProductBenefits = props => {
     <section className="product-benefits">
       <div className="product-benefits-group">
         <div className="product-benefit-1-tagline">
-          Here is product benefit 1. Isn't it amazing?
-        </div>
+Landing pages, Content management systems , Learning managent systems.
+    </div>
         <div className="product-benefit-1-image"></div>
         <div className="product-benefit-2-tagline">
-          Here is product benefit 2. I can do this all day long.
-        </div>
+Native web apps and Native app for Andriod and iOS and mobile friendly responsive design.
+    </div>
         <div className="product-benefit-2-image"></div>
         <div className="product-benefit-3-tagline">
-          Boom. Product benefit 3. Kills any last remaining doubts no?
-        </div>
+SEO, Digital and social media marketing solutions .
+    </div>
         <div className="product-benefit-3-image"></div>
       </div>
     </section>
