18737a1360b20cc9a239054ca00b360d4e96833c
PradyumnNair <51204441+PradyumnNair@users.noreply.github.com>
2020-08-10 21:14:23 +0530

    Update CallToAction.js

--- src/components/CallToAction.js
+++ src/components/CallToAction.js
@@ -5,13 +5,12 @@ import Button from "./Button";
 const CallToAction = props => {
 
   return (
-    <section className="cta">
+    <section className="cta" id={'partner'}>
     
-   <section className="Partner">
       <h1 classID="Partner">Partner with us to create amazing digital solutions</h1>
       <p>One stop destination for building and delivering your ideas into reality</p>
       <a target="_blank" href="https://wa.me/917989854956"> <Button label="Get Started" /></a>
-    </section></section>
+    </section>
   )
 };
 
