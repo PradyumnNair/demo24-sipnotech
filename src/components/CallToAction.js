b73ed721c7ded08721bdaebda1f0fecb18fa027c
PradyumnNair <51204441+PradyumnNair@users.noreply.github.com>
2020-08-10 20:11:00 +0530

    Update CallToAction.js

--- src/components/CallToAction.js
+++ src/components/CallToAction.js
@@ -9,7 +9,7 @@ const CallToAction = props => {
    <section className="Partner">
       <h1>Partner with us to create amazing digital solutions</h1>
       <p>One stop destination for building and delivering your ideas into reality</p>
-      <Button  onclick="window.location.href='https://wa.me/917989854956'" label="Get Started" />
+      <Button  href="https://wa.me/917989854956" label="Get Started" />
     </section></section>
   )
 };
