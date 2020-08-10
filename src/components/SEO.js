14255cd8b01995187305d2da23107b246c40a914
PradyumnNair <51204441+PradyumnNair@users.noreply.github.com>
2020-08-10 19:13:17 +0530

    Update SEO.js

--- src/components/SEO.js
+++ src/components/SEO.js
@@ -29,8 +29,8 @@ function SEO({ description, lang, meta, title }) {
       titleTemplate={`%s | ${site.siteMetadata.title}`}
       meta={[
         {
-          name: `title`,
-          content: We create IT solutions for small businesses and startups,
+          name: `description`,
+          content: metaDescription,
         },
         {
           property: `og:title`,
