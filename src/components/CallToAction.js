53c6b7ea3146a0259bf1dae2ba1ae7dcd665dd53
PradyumnNair <51204441+PradyumnNair@users.noreply.github.com>
2020-08-10 20:31:52 +0530

    Update CallToAction.js

--- src/components/CallToAction.js
+++ src/components/CallToAction.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from "gatsby";
 import Button from "./Button";
 
 const CallToAction = props => {
@@ -9,7 +10,7 @@ const CallToAction = props => {
    <section className="Partner">
       <h1>Partner with us to create amazing digital solutions</h1>
       <p>One stop destination for building and delivering your ideas into reality</p>
-      <Button url="https://wa.me/917989854956" label="Get Started" />
+      <Link to="https://wa.me/917989854956"> <Button label="Get Started" /></Link>
     </section></section>
   )
 };
