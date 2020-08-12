b2ca3e4f547c1fa86d3993c20676d1f754d3cddc
PradyumnNair <51204441+PradyumnNair@users.noreply.github.com>
2020-08-12 19:03:42 +0530

    Update ProductBenefits.js

--- src/components/ProductBenefits.js
+++ src/components/ProductBenefits.js
@@ -6,18 +6,18 @@ const ProductBenefits = props => {
     <section className="product-benefits" id={"services"}>
       <div className="product-benefits-group">
         <div className="product-benefit-1-tagline">
-          Web development <h4>
-Landing pages, Content management systems , Learning managent systems.</h4>
+          Web development <h6>
+Landing pages, Content management systems , Learning managent systems.</h6>
     </div>
         <div className="product-benefit-1-image"></div>
         <div className="product-benefit-2-tagline">
-          Mobile development <h4>
-Native web apps and Native app for Andriod and iOS and mobile friendly responsive design.</h4>
+          Mobile development <h6>
+Native web apps and Native app for Andriod and iOS and mobile friendly responsive design.</h6>
     </div>
         <div className="product-benefit-2-image"></div>
         <div className="product-benefit-3-tagline">
-          Digital reach <h4>
-SEO, Digital and social media marketing solutions .</h4>
+          Digital reach <h6>
+SEO, Digital and social media marketing solutions .</h6>
     </div>
         <div className="product-benefit-3-image"></div>
       </div>
